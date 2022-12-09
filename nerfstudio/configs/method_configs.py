947aee0313fa494fb1fe192d6a31d545abb69621
HarrisonLeitch <harrisonleitch1@gmail.com>
2022-12-09 16:23:26 +0000

    Back to adam

--- nerfstudio/configs/method_configs.py
+++ nerfstudio/configs/method_configs.py
@@ -337,7 +337,11 @@ method_configs["hleitchnerf"] = Config(
     trainer=TrainerConfig(
         steps_per_eval_batch=500, steps_per_save=2000, max_num_iterations=30000, mixed_precision=True
     ),
+<<<<<<< refs/remotes/origin/main
     pipeline=VanillaPipelineConfig(        
+=======
+    pipeline=DynamicBatchPipelineConfig(        
+>>>>>>> Back to adam
             datamanager=VanillaDataManagerConfig(
             dataparser=NerfstudioDataParserConfig(),
             train_num_rays_per_batch=4096,
