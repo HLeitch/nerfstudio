a935a18ef16835f8c59daf3eb1479c0a35ba6c61
HarrisonLeitch <harrisonleitch1@gmail.com>
2022-12-12 11:41:38 +0000

    fixed and Hleitchnerf reset

--- nerfstudio/configs/method_configs.py
+++ nerfstudio/configs/method_configs.py
@@ -337,7 +337,7 @@ method_configs["hleitchnerf"] = Config(
     trainer=TrainerConfig(
         steps_per_eval_batch=500, steps_per_save=2000, max_num_iterations=30000, mixed_precision=True
     ),
-    pipeline=DynamicBatchPipelineConfig(        
+    pipeline=VanillaPipelineConfig(        
             datamanager=VanillaDataManagerConfig(
             dataparser=NerfstudioDataParserConfig(),
             train_num_rays_per_batch=4096,
