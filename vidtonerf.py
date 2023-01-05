88cf14d65316e1ef00087ff41ed753a58b39d8c3
HarrisonLeitch <harrisonleitch1@gmail.com>
2023-01-05 11:33:15 +0000

    Auto stash before rebase of "refs/heads/hleitchnerf"

--- vidtonerf.py
+++ vidtonerf.py
@@ -14,8 +14,8 @@ def recursiveSearch(directory, videoList):
             videoList.append(fullLocation)
 
         if "." not in file:
-
-            recursiveSearch(fullLocation, videoList)
+            continue
+            ##recursiveSearch(fullLocation, videoList)
     return videoList
 
 
@@ -24,6 +24,8 @@ if __name__ == "__main__":
     root_folder = input()
     print("Which NeRF method?: ")
     method = input()
+    print("Identifier:")
+    ident = input()
 
     videoFiles = list()
 
@@ -36,21 +38,21 @@ if __name__ == "__main__":
         directory = os.path.dirname(video)
         if "colmap" not in os.listdir(directory):
             os.system(
-                f"ns-process-data video --data {video} --output-dir {directory} --matching-method sequential --verbose --num-frames-target 300"
+                f"ns-process-data video --data {video} --output-dir {directory} --matching-method sequential --verbose --num-frames-target 300 "
             )
 
     for video in videoFiles:
         directory = os.path.dirname(video)
-        timestamp = method + os.path.basename(video)[0:-4]
+        timestamp = method + os.path.basename(video)[0:-4]+ ident
 
         os.curdir = directory
         print(f"Training Start: {directory}")
         os.system(
-            f"ns-train {method} --data {directory} --output-dir .\\ --timestamp {timestamp} --viewer.quit-on-train-completion True --trainer.max-num-iterations 10000"
+            f"ns-train {method} --data {directory} --output-dir .\\ --timestamp {timestamp} --viewer.quit-on-train-completion True --trainer.max-num-iterations 10000 --vis tensorboard"
         )
 
         print("Training Finished")
-        os.system(f"ns-eval --load-config {directory}\\{method}\\{timestamp}\\config.yml --output-path {directory}")
+        os.system(f"ns-eval --load-config {directory}\\{method}\\{timestamp}\\config.yml --output-path {directory}\\{timestamp}.json")
         print("Evaluation Finished")
         os.system(
             f"ns-render --load-config {directory}\\{method}\\{timestamp}\\config.yml --traj filename --camera-path-filename C:\\Users\\hleit\\Documents\\nerfstudio\\CamPath\\DecayCamPath.json --seconds 15 --output-path {directory}\\{method}\\{timestamp}.mp4"
