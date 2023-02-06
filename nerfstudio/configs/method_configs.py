41fadad8b82a9abb51b8095471a4004fc3e875e9
HarrisonLeitch <harrisonleitch1@gmail.com>
2023-02-06 14:50:45 +0000

    New mesh exporter started

--- nerfstudio/configs/method_configs.py
+++ nerfstudio/configs/method_configs.py
@@ -82,8 +82,7 @@ descriptions = {
     "tensorf": "tensorf",
     "dnerf": "Dynamic-NeRF model. (slow)",
     "phototourism": "Uses the Phototourism data.",
-    "hleitchnerf": "Experimental changes to nerfacto. P.S. you probably have the wrong repo"
-
+    "hleitchnerf": "Experimental changes to nerfacto. P.S. you probably have the wrong repo",
     "nerfplayer-nerfacto": "NeRFPlayer with nerfacto backbone.",
     "nerfplayer-ngp": "NeRFPlayer with InstantNGP backbone.",
 
@@ -344,46 +343,6 @@ method_configs["phototourism"] = TrainerConfig(
     viewer=ViewerConfig(num_rays_per_chunk=1 << 15),
     vis="viewer",
 )
-method_configs["hleitchnerf"] = Config(
-    method_name="hleitchnerf",
-    trainer=TrainerConfig(
-        steps_per_eval_batch=500, steps_per_save=2000, max_num_iterations=30000, mixed_precision=True
-    ),
-
-    pipeline=VanillaPipelineConfig(        
-
-    pipeline=DynamicBatchPipelineConfig(        
-
-
-    pipeline=VanillaPipelineConfig(        
-
-    pipeline=VanillaPipelineConfig(        
-
-            datamanager=VanillaDataManagerConfig(
-            dataparser=NerfstudioDataParserConfig(),
-            train_num_rays_per_batch=4096,
-            eval_num_rays_per_batch=4096,
-            camera_optimizer=CameraOptimizerConfig(
-                mode="SO3xR3", optimizer=AdamOptimizerConfig(lr=6e-4, eps=1e-8, weight_decay=1e-2)
-            ),
-        ),
-        model=hleitchnerfModelConfig(eval_num_rays_per_chunk=1 << 15),
-
-    ),
-    optimizers={
-        "proposal_networks": {
-            "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
-            "scheduler": None,
-        },
-        "fields": {
-            "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
-            "scheduler": None,
-        },
-    },
-    viewer=ViewerConfig(num_rays_per_chunk=1 << 15),
-    vis="viewer",
-)
-
 method_configs["nerfplayer-nerfacto"] = TrainerConfig(
     method_name="nerfplayer-nerfacto",
     steps_per_eval_batch=500,
@@ -415,8 +374,6 @@ method_configs["nerfplayer-nerfacto"] = TrainerConfig(
     viewer=ViewerConfig(num_rays_per_chunk=1 << 15),
     vis="viewer",
 )
-
-
 method_configs["nerfplayer-ngp"] = TrainerConfig(
     method_name="nerfplayer-ngp",
     steps_per_eval_batch=500,
