8817090ab1ef333acd2519ccbdc3e5f634b8757d
HarrisonLeitch <harrisonleitch1@gmail.com>
2023-11-13 15:47:49 +0000

    Testing changes for rmse value and completed pointcloud conversion. See book.

--- hausdorfftest.py
+++ hausdorfftest.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import point_cloud_utils as pcu
 import pytorch3d as torch3d
+import pytorch3d.io as torchio
 import pytorch3d.ops as torchops
 from pytorch3d.structures import Pointclouds
 import torch as torch
@@ -69,7 +70,7 @@ def IterationTest(file_a, file_b, num_points):
     b_full_pc = Pointclouds(b_tensor_full[None,:,:])
     #%%
     
-    output = torchops.iterative_closest_point(a_pc, b_pc,verbose=True,max_iterations=500)
+    output = torchops.iterative_closest_point(a_pc, b_pc,verbose=True,max_iterations=500, relative_rmse_thr=1e-7)
     rmse = output.rmse
     transformed_a = output.Xt
     RTs = output.RTs
@@ -131,13 +132,18 @@ def IterationTest(file_a, file_b, num_points):
     # hausdorff_dist, idx_b, idx_a = pcu.hausdorff_distance(b, transformed_a, return_index=True)
     # assert np.abs(np.sum((a[idx_a] - b[idx_b])**2) - hausdorff_dist**2) < 1e-5, "These values should be almost equal"  
     # print(f"Hausdorff max: {hausdorff_a_to_b}")
+    
+    ##Save rotated point cloud
+    pcu.save_mesh_v((file_a[:-4]+f"matched{num_points}.obj"), a_full_pc[0].cpu().numpy())
 
     return rmse.cpu().numpy(), reduced_pc_comparison, full_pc_comparison, general_hausdorff, time_taken, iterations_taken
 #%%
 results = pandas.DataFrame([],columns=['num_points','rmse','ICP_hausdorff','Full_1D_hausdorff','Full_2D_hausdorff','time_taken','iterations'])
 
-for num_points in [100, 200, 500, 750, 1000, 1500, 2000, 5000, 7500, 10000, 15000, 20000]:
-    rmse, reduced_pc_comparison, full_pc_comparison, general_hausdorff, time_taken, iterations_taken =IterationTest("./data/tandt/Ignatius/ignatius_base.obj", "./data/tandt/Ignatius/Ignatius_z_rot.obj", num_points) ##IterationTest("./data/tandt/Caterpillar/Caterpillar_base.obj", "./data/tandt/Caterpillar/Caterpillar_shifted.obj", num_points)
+for num_points in [1000,1001,1002,1003,1004,1005]:
+    rmse, reduced_pc_comparison, full_pc_comparison, general_hausdorff, time_taken, iterations_taken = IterationTest( "./data/nerf-synthetic/lego/lego_pointcloud_rotated.obj", "./data/nerf-synthetic/lego/lego_pointcloud.obj", num_points)
+    ##IterationTest( "./data/nerf-synthetic/lego/lego_pointcloud_rotated.obj", "./data/nerf-synthetic/lego/lego_pointcloud.obj", num_points)
+    ##IterationTest("./data/tandt/Ignatius/Ignatius_z_rot.obj","./data/tandt/Ignatius/ignatius_base.obj",  num_points) ##IterationTest( "./data/tandt/Caterpillar/Caterpillar_shifted.obj","./data/tandt/Caterpillar/Caterpillar_base.obj", num_points)
     
     new_row = {'num_points':num_points,'rmse':rmse, 'ICP_hausdorff':reduced_pc_comparison, 'Full_1D_hausdorff':full_pc_comparison, 'Full_2D_hausdorff':general_hausdorff, 'time_taken': time_taken, 'iterations': iterations_taken}
     
